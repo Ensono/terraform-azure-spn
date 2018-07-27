e769afdeededecac92de8462938cb72b5a18a29b
William Ayerst <williamayerst@AML0131.local>
2018-07-27 16:04:23 +0100

    revert

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "spn_objectid" {
-  value = "${azurerm_azuread_service_principal.spn.*.id ? element(azurerm_azuread_service_principal.spn.*.id,0) : "" }"
+  value = "${azurerm_azuread_service_principal.spn.*.id}"
 }
 
 output "spn_passwordguid" {
-  value = "${azurerm_azuread_service_principal_password.spn.*.id ? element(azurerm_azuread_service_principal_password.spn.*.id,0) : "" }"
+  value = "${azurerm_azuread_service_principal_password.spn.*.id}"
 }
 
 output "spn_applicationid" {
-  value = "${azurerm_azuread_application.spn.*.application_id ? element(azurerm_azuread_application.spn.*.application_id,0) : "" }"
+  value = "${azurerm_azuread_application.spn.*.application_id}"
 }
