0890bda8bc5095d637a91cc227d5e86cd1110ec1
William Ayerst <williamayerst@AML0131.local>
2018-07-27 16:20:14 +0100

    shame #2

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "spn_objectid" {
-  value = "${var.create_akscluster ? azurerm_azuread_service_principal.spn.0.id : ""}"
+  value = "${var.create_resource ? azurerm_azuread_service_principal.spn.0.id : ""}"
 }
 
 output "spn_passwordguid" {
-  value = "${var.create_akscluster ? azurerm_azuread_service_principal_password.spn.0.id : "" }"
+  value = "${var.create_resource ? azurerm_azuread_service_principal_password.spn.0.id : "" }"
 }
 
 output "spn_applicationid" {
-  value = "${var.create_akscluster ? azurerm_azuread_application.spn.0.application_id : "" }"
+  value = "${var.create_resource ? azurerm_azuread_application.spn.0.application_id : "" }"
 }
