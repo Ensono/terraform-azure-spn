ff05a03155e4df7a714a4a924660a9864d2ea8d4
William Ayerst <williamayerst@AML0131.local>
2018-07-27 15:47:37 +0100

    adjusted splat to first element, on outputs

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "spn_objectid" {
-  value = "${azurerm_azuread_service_principal.spn.*.id}"
+  value = "${azurerm_azuread_service_principal.spn.0.id}"
 }
 
 output "spn_passwordguid" {
-  value = "${azurerm_azuread_service_principal_password.spn.*.id}"
+  value = "${azurerm_azuread_service_principal_password.spn.0.id}"
 }
 
 output "spn_applicationid" {
-  value = "${azurerm_azuread_application.spn.*.application_id}"
+  value = "${azurerm_azuread_application.spn.0.application_id}"
 }
