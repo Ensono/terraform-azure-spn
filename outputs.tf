995f1c8c0ba5f944e5e3511435aaf399017dd1fa
William Ayerst <williamayerst@AML0131.local>
2018-07-27 15:56:07 +0100

    two strings

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "spn_objectid" {
-  value = "${var.create_resource ? azurerm_azuread_service_principal.spn.*.id : 0 }"
+  value = "${var.create_resource ? element(azurerm_azuread_service_principal.spn.*.id,0) : "" }"
 }
 
 output "spn_passwordguid" {
-  value = "${var.create_resource ? azurerm_azuread_service_principal_password.spn.*.id : 0 }"
+  value = "${var.create_resource ? element(azurerm_azuread_service_principal_password.spn.*.id,0) : "" }"
 }
 
 output "spn_applicationid" {
-  value = "${var.create_resource ? azurerm_azuread_application.spn.*.application_id : 0 }"
+  value = "${var.create_resource ? element(azurerm_azuread_application.spn.*.application_id,0) : "" }"
 }
