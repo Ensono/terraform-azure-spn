45caa866805cf5b2bc3de8ce80ef3cf550b88334
William Ayerst <williamayerst@AML0131.local>
2018-07-27 14:18:10 +0100

    renamed expiry parameter in main manifest, added conditional create

--- main.tf
+++ main.tf
@@ -3,21 +3,25 @@
 ##################################################
 
 resource "random_string" "spn_password" {
+  count   = "${var.create_resource ? 1 : 0 }"
   length  = 16
   special = true
 }
 
 resource "azurerm_azuread_application" "spn" {
+  count    = "${var.create_resource ? 1 : 0 }"
   name     = "${var.spn_name}"
   homepage = "${var.app_url}"
 }
 
 resource "azurerm_azuread_service_principal" "spn" {
+  count          = "${var.create_resource ? 1 : 0 }"
   application_id = "${azurerm_azuread_application.spn.application_id}"
 }
 
 resource "azurerm_azuread_service_principal_password" "spn" {
+  count                = "${var.create_resource ? 1 : 0 }"
   service_principal_id = "${azurerm_azuread_service_principal.spn.id}"
   value                = "${var.generate_password ? random_string.spn_password.result : var.spn_password}"
-  end_date             = "${var.spn_enddate}"
+  end_date             = "${var.spn_expiry}"
 }
