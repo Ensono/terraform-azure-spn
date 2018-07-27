61ef3f332c16c54d5ae37126c5a61650102efaae
William Ayerst <williamayerst@AML0131.local>
2018-07-27 15:54:26 +0100

    splat

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "spn_objectid" {
-  value = "${var.create_resource ? azurerm_azuread_service_principal.spn.0.id : 0 }"
+  value = "${var.create_resource ? azurerm_azuread_service_principal.spn.*.id : 0 }"
 }
 
 output "spn_passwordguid" {
-  value = "${var.create_resource ? azurerm_azuread_service_principal_password.spn.0.id : 0 }"
+  value = "${var.create_resource ? azurerm_azuread_service_principal_password.spn.*.id : 0 }"
 }
 
 output "spn_applicationid" {
-  value = "${var.create_resource ? azurerm_azuread_application.spn.0.application_id : 0 }"
+  value = "${var.create_resource ? azurerm_azuread_application.spn.*.application_id : 0 }"
 }
