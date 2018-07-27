e72e1420b49b47762606655b360410158ca5769a
William Ayerst <williamayerst@AML0131.local>
2018-07-27 16:58:46 +0100

    workaround 2

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,16 @@
 output "spn_objectid" {
   # value = "${azurerm_azuread_service_principal.spn.*.id}"
-  value = "${element(concat(azurerm_azuread_service_principal.spn.0.id, list("")), 0)}"
+  value = "${element(concat(azurerm_azuread_service_principal.spn.*.id, list("")), 0)}"
 }
 
 output "spn_passwordguid" {
-  value = "${element(concat(azurerm_azuread_service_principal_password.spn.0.id, list("")), 0)}"
+  value = "${element(concat(azurerm_azuread_service_principal_password.spn.*.id, list("")), 0)}"
 
   #  value = "${azurerm_azuread_service_principal_password.spn.*.id}"
 }
 
 output "spn_applicationid" {
-  value = "${element(concat(azurerm_azuread_application.spn.0.application_id, list("")), 0)}"
+  value = "${element(concat(azurerm_azuread_application.spn.*.application_id, list("")), 0)}"
 
   # value = "${azurerm_azuread_application.spn.*.application_id}"
 }
