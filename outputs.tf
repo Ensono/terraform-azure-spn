42cb0dc71ce14dd78b2b7b11fe8041f3dc2c51d2
William Ayerst <williamayerst@AML0131.local>
2018-07-27 16:18:32 +0100

    conditional, flat output

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "spn_objectid" {
-  value = "${azurerm_azuread_service_principal.spn.*.id}"
+  value = "${var.create_akscluster ? azurerm_azuread_service_principal.spn.id : ""}"
 }
 
 output "spn_passwordguid" {
-  value = "${azurerm_azuread_service_principal_password.spn.*.id}"
+  value = "${var.create_akscluster ? azurerm_azuread_service_principal_password.spn.id : "" }"
 }
 
 output "spn_applicationid" {
-  value = "${azurerm_azuread_application.spn.*.application_id}"
+  value = "${var.create_akscluster ? azurerm_azuread_application.spn.application_id : "" }"
 }
