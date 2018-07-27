2be044c580ff65e529ceb92ebf9fd5197b25fd06
William Ayerst <williamayerst@AML0131.local>
2018-07-27 16:57:53 +0100

    potential workaround

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,16 @@
 output "spn_objectid" {
-  value = "${azurerm_azuread_service_principal.spn.*.id}"
+  # value = "${azurerm_azuread_service_principal.spn.*.id}"
+  value = "${element(concat(azurerm_azuread_service_principal.spn.0.id, list("")), 0)}"
 }
 
 output "spn_passwordguid" {
-  value = "${azurerm_azuread_service_principal_password.spn.*.id}"
+  value = "${element(concat(azurerm_azuread_service_principal_password.spn.0.id, list("")), 0)}"
+
+  #  value = "${azurerm_azuread_service_principal_password.spn.*.id}"
 }
 
 output "spn_applicationid" {
-  value = "${azurerm_azuread_application.spn.*.application_id}"
+  value = "${element(concat(azurerm_azuread_application.spn.0.application_id, list("")), 0)}"
+
+  # value = "${azurerm_azuread_application.spn.*.application_id}"
 }
