bb1af04822bd2114504d5f985144df4ef04ad09c
William Ayerst <williamayerst@AML0131.local>
2018-07-27 15:53:24 +0100

    conditional outputs

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,11 @@
 output "spn_objectid" {
-  value = "${azurerm_azuread_service_principal.spn.id}"
+  value = "${var.create_resource ? azurerm_azuread_service_principal.spn.0.id : 0 }"
 }
 
 output "spn_passwordguid" {
-  value = "${azurerm_azuread_service_principal_password.spn.id}"
+  value = "${var.create_resource ? azurerm_azuread_service_principal_password.spn.0.id : 0 }"
 }
 
 output "spn_applicationid" {
-  value = "${azurerm_azuread_application.spn.application_id}"
+  value = "${var.create_resource ? azurerm_azuread_application.spn.0.application_id : 0 }"
 }
