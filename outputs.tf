71ee422b9e125b4d68bebaa17ba411ac47b9a83c
William Ayerst <william.ayerst@amido.com>
2019-06-10 10:05:29 +0100

    Fix AzureAD provider version for HCL 0.12 #2

--- outputs.tf
+++ outputs.tf
@@ -1,16 +1,16 @@
 output "spn_objectid" {
-  # value = "${azurerm_azuread_service_principal.spn.*.id}"
-  value = "${element(concat(azurerm_azuread_service_principal.spn.*.id, list("")), 0)}"
+  # value = "${azuread_service_principal.spn.*.id}"
+  value = "${element(concat(azuread_service_principal.spn.*.id, list("")), 0)}"
 }
 
 output "spn_passwordguid" {
-  value = "${element(concat(azurerm_azuread_service_principal_password.spn.*.id, list("")), 0)}"
+  value = "${element(concat(azuread_service_principal_password.spn.*.id, list("")), 0)}"
 
-  #  value = "${azurerm_azuread_service_principal_password.spn.*.id}"
+  #  value = "${azuread_service_principal_password.spn.*.id}"
 }
 
 output "spn_applicationid" {
-  value = "${element(concat(azurerm_azuread_application.spn.*.application_id, list("")), 0)}"
+  value = "${element(concat(azuread_application.spn.*.application_id, list("")), 0)}"
 
-  # value = "${azurerm_azuread_application.spn.*.application_id}"
+  # value = "${azuread_application.spn.*.application_id}"
 }
