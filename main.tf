e289c273bb825d844a7a7acd10f2ea977231bd23
William Ayerst <william.ayerst@amido.com>
2019-06-10 10:01:58 +0100

    Change to AzureAD provider

--- main.tf
+++ main.tf
@@ -1,6 +1,9 @@
 ##################################################
 ##  AKS SPN
 ##################################################
+provider "azuread" {
+  version = "=0.1.0"
+}
 
 resource "random_string" "spn_password" {
   count   = "${var.create_resource ? 1 : 0 }"
@@ -8,20 +11,20 @@ resource "random_string" "spn_password" {
   special = true
 }
 
-resource "azurerm_azuread_application" "spn" {
+resource "azuread_application" "spn" {
   count    = "${var.create_resource ? 1 : 0 }"
   name     = "${var.spn_name}"
   homepage = "${var.spn_url}"
 }
 
-resource "azurerm_azuread_service_principal" "spn" {
+resource "azuread_service_principal" "spn" {
   count          = "${var.create_resource ? 1 : 0 }"
-  application_id = "${azurerm_azuread_application.spn[0].application_id}"
+  application_id = "${azuread_application.spn[0].application_id}"
 }
 
-resource "azurerm_azuread_service_principal_password" "spn" {
+resource "azuread_service_principal_password" "spn" {
   count                = "${var.create_resource ? 1 : 0 }"
-  service_principal_id = "${azurerm_azuread_service_principal.spn[0].id}"
+  service_principal_id = "${azuread_service_principal.spn[0].id}"
   value                = "${var.generate_password ? random_string.spn_password[0].result : var.spn_password}"
   end_date             = "${var.spn_expiry}"
 }
